18cf4677f1d871568396597a6f28a824456a87aa
Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>
2020-09-27 19:43:39 +0200

    added preview

--- art.js
+++ art.js
@@ -5,12 +5,15 @@ var path = require("path")
 app.get("/", async (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"))
 })
-
 app.get("/:num", async (req, res) => {
     res.sendFile(path.join(__dirname + "/" + req.params.num + "/index.html"))
 })
 app.get("/:num/artJs.js", async (req, res) => {
     res.sendFile(path.join(__dirname + "/" + req.params.num + "/artJs.js"))
+})
+
+app.get("/:num/screenshot", async (req, res) => {
+    res.sendFile(path.join(__dirname + "/screenshot/" + req.params.num + "jpg"))
 })
 
 app.listen(3009)
